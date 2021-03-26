e0c067039237a920bad0606f47acdb1a95c43534
Shajia Abidi <abidishajia@gmail.com>
2021-03-26 18:32:59 -0500

    Added Firebase configuration to the project.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var firebaseConfig = {
-  apiKey: { TCL_X_API_KEY },
-  authDomain: { TCL_X_AUTH_DOMAIN },
-  databaseURL: { TCL_X_DATABASE_URL },
-  projectId: { TCL_X_PROJECT_ID },
-  storageBucket: { TCL_X_STORAGE_BUCKET },
-  messagingSenderId: { TCL_X_SENDER_ID },
-  appId: { TCL_X_APP_ID },
+    apiKey: "AIzaSyBiGTyF01ZsVHAyvBv9GidY14qjHFo0I64",
+    authDomain: "tcl-24-shopping-list.firebaseapp.com",
+    projectId: "tcl-24-shopping-list",
+    storageBucket: "tcl-24-shopping-list.appspot.com",
+    messagingSenderId: "545115595707",
+    appId: "1:545115595707:web:329d1e8bd51d53e7c75a9e"
+
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
