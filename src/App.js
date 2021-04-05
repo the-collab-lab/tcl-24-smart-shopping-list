ef4894d65ee4eab0e12b1ae6ab07624c991e1e23
Ana Mafla <anamafla@hotmail.com>
2021-04-05 18:19:48 -0500

    Set up router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import './App.css';
 import AddItem from './components/AddItem';
 import List from './components/List';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <List />
-      <AddItem />
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/list">List</Link>
+
+        <Link to="/addItem">Add Item</Link>
+
+        <Switch>
+          <Route path="/list">
+            <List />
+          </Route>
+          <Route path="/addItem">
+            <AddItem />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
