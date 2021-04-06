b54b26328912d00dede4ccfa0d46480702786469
candrepa1 <candrepa1@gmail.com>
2021-04-06 15:30:50 -0500

    get from firebase

---
+++ src/components/Data.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { fb } from '../lib/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+const Data = () => {
+  const [value, loading, error] = useCollection(
+    fb.firestore().collection('things'),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    },
+  );
+
+  return (
+    <div>
+      {error && <strong>Error: {JSON.stringify(error)}</strong>}
+      {loading && <span>Collection: Loading...</span>}
+      {value && (
+        <ul>
+          {value.docs.map((doc) => (
+            <li key={doc.id}>{doc.data().name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Data;
