153b94350a2fb4a3d093b1d0d94c9b86cdd8fed7
Ana Mafla <anamafla@hotmail.com>
2021-04-09 14:42:23 -0500

    deleted curly brackets and organized imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import AddItem from './components/AddItem/AddItem.js';
 import List from './components/List/List.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar.js';
 
 function App() {
