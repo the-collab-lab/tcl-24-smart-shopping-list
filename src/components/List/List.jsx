4089ec15bb0a39d467f524fb5dc0baedca82d176
candrepa1 <candrepa1@gmail.com>
2021-04-26 16:14:21 -0500

    created checkbox for item and controlled the input

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { useFirebase } from '../../hooks/useFirebase';
 
@@ -11,6 +11,8 @@ const List = () => {
 
   const [value, loading, error] = useCollection(firebasePath);
 
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div>
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
@@ -18,7 +20,14 @@ const List = () => {
       {value && (
         <ul>
           {value.docs.map((doc) => (
-            <li key={doc.id}>{doc.data().name}</li>
+            <li key={doc.id}>
+              <input
+                type="checkbox"
+                value={isChecked}
+                onChange={() => setIsChecked(!isChecked)}
+              />
+              {doc.data().name}
+            </li>
           ))}
         </ul>
       )}
